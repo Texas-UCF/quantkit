636b6a08b44c511ef7bcf19cbaae2200399c15dd
vishalv95 <vishalv95@gmail.com>
2015-09-20 19:06:45 -0500

    Implemented LargeMoves. Uses intraday movements to track days that had large movements

---
+++ R/LargeMoves.R
@@ -0,0 +1,25 @@
+#' Large Moves.
+#'
+#' @param ticker Yahoo Finance target ticker
+#' @param k number of standard deviations to be considered a large move
+#' @param start Start date
+#' @param end End date
+#' @return price series of days that had large moves
+#' @keywords regression, components
+#' @importFrom quantmod getSymbols dailyReturn
+#' @export
+#' @examples
+#' LargeMoves("XOM", 2, '2015-01-01', '2015-05-25')
+
+LargeMoves <- function(ticker, k, start=Sys.Date() - 365, end=Sys.Date()){
+    tickerData <- new.env()
+    getSymbols(ticker,from=start,to=end,env=tickerData)
+    
+    tickerDaily <- get(ticker, tickerData)
+    
+    dailyMoves <- tickerDaily[,2] - tickerDaily[,3]
+    dailySD <- sd(dailyMoves)
+    largeMoves <- dailyMoves[dailyMoves > k*dailySD,]
+    
+    return(tickerDaily[which(index(tickerDaily) %in% index(largeMoves)),])
+}
