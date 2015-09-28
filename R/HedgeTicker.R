82a739ae6276905b1b16eca99c88f52faeda4f01
vishalv95 <vishalv95@gmail.com>
2015-09-28 14:28:21 -0500

    Implemented function for single ticker against multiple components because anything else is too hard and confusing

---
+++ R/HedgeTicker.R
@@ -0,0 +1,21 @@
+#' Hedge Portfolio
+#'
+#' @param ticker Yahoo Finance target ticker
+#' @param numShares number of shares in ticker 
+#' @param components Components that you want to have 0 exposure to when you hedge
+#' @param cutoff If p-value > cutoff, exclude the portfolio component
+#' @param start Start date
+#' @param end End date
+#' @return number of shares of each component necessary to hedge out exposure
+#' @keywords regression, components
+#' @importFrom quantmod getSymbols
+#' @export
+#' @examples
+#' HedgeTicker('SDRL', 100, c('SPY', 'USO'))
+
+HedgeTicker <- function(ticker, numshares, components='^GSPC', cutoff=0.05, start=Sys.Date()-365, end=Sys.Date()){
+  filter <- FilterComponents(ticker, components=components, 
+                               cutoff = cutoff, start=start, end=end)
+  filter <- filter$regression$coefficients[,1] * -1 * numshares
+  return(filter[2:length(filter)])
+}
