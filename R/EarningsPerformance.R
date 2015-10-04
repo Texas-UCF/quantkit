62211353cc873ed4e111ec57fa800ba9ac2a7e02
kevinoyu <kevinoyu@gmail.com>
2015-10-04 13:17:31 -0500

    Fix bug in EarningsPerformance with search dates

--- R/EarningsPerformance.R
+++ R/EarningsPerformance.R
@@ -12,37 +12,39 @@
 #' EarningsPerformance("TSLA", 2, 2015)
 
 EarningsPerformance <- function(ticker, quarterNumber, year) {
-# # We want to search in the quarter ahead of the quarter reported by the earnings announcment
-# # for the date.
-#    searchStart<-as.Date(Sys.Date())
-#    searchEnd<-as.Date(Sys.Date())
-#    if(quarterNumber == 3) {
-#       searchStart <- as.Date(paste(year - 1, "-10-01", sep = ""))
-#       searchEnd <- as.Date(paste(year - 1, "-12-31", sep = ""))
-#    }
-#    else if(quarterNumber == 4) {
-#       searchStart <- as.Date(paste(year + 1, "-01-01", sep = ""))
-#       searchEnd <- as.Date(paste(year + 1, "-03-31", sep = ""))
-#    }
-#    else if(quarterNumber == 1) {
-#      searchStart <- as.Date(paste(year, "-04-01", sep = ""))
-#      searchEnd <- as.Date(paste(year, "-06-30", sep = ""))
-#    }
-#    else if(quarterNumber == 2) {
-#      searchStart <- as.Date(paste(year, "-07-01", sep = ""))
-#      searchStart <- as.Date(paste(year, "-09-30", sep = ""))
-#    }
-#
-#    # Search the earnings calendar using qmao during that period
-#    ec <- getEarningsCalendar(from = searchStart, to = searchEnd)
-#    indexFound <- match(ticker, ec[,"Symbol"], nomatch = -1)
-#    if(indexFound == -1) {
-#      stop("Earnings report not found for that quarter")
-#    }
-#    earningsAnnouncement <- as.Date(ec[indexFound, "Date"])
-#
-#    returns <- EventMovement(ticker, earningsAnnouncement)
-#
-#  # Display a time series plot of the stock's performance
-#  plot(returns, y = NULL, xlab = "Time", ylab = "Log daily returns", main = paste(ticker, earningsAnnouncement, "earnings report performance"))
+# We want to search in the quarter ahead of the quarter reported by the earnings announcment
+# for the date.
+
+   searchStart<-as.Date(Sys.Date())
+   searchEnd<-as.Date(Sys.Date())
+
+   if(quarterNumber == 3) {
+      searchStart <- as.Date(paste(year - 1, "-10-01", sep = ""))
+      searchEnd <- as.Date(paste(year - 1, "-12-31", sep = ""))
+   }
+   else if(quarterNumber == 4) {
+      searchStart <- as.Date(paste(year + 1, "-01-01", sep = ""))
+      searchEnd <- as.Date(paste(year + 1, "-03-31", sep = ""))
+   }
+   else if(quarterNumber == 1) {
+     searchStart <- as.Date(paste(year, "-04-01", sep = ""))
+     searchEnd <- as.Date(paste(year, "-06-30", sep = ""))
+   }
+   else if(quarterNumber == 2) {
+     searchStart <- as.Date(paste(year, "-07-01", sep = ""))
+     searchEnd <- as.Date(paste(year, "-09-30", sep = ""))
+   }
+
+   # Search the earnings calendar using qmao during that period
+   ec <- getEarningsCalendar(from = searchStart, to = searchEnd)
+   indexFound <- match(ticker, ec[,"Symbol"], nomatch = -1)
+   if(indexFound == -1) {
+     stop("Earnings report not found for that quarter")
+   }
+   earningsAnnouncement <- as.Date(ec[indexFound, "Date"])
+
+   returns <- EventMovement(ticker, earningsAnnouncement)
+
+ # Display a time series plot of the stock's performance
+ plot(returns[[1]], y = NULL, xlab = "Time", ylab = "Log daily returns", main = paste(ticker, earningsAnnouncement, "earnings report performance"))
 }
