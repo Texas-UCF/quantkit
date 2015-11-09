7fe2bfa79f388fcd5d6ac9beb2a18e778934891a
kevinoyu <kevinoyu@gmail.com>
2015-11-09 15:21:40 -0600

    Fix error in pairs backtest

--- R/PairsBacktest.R
+++ R/PairsBacktest.R
@@ -15,7 +15,7 @@
 PairsBacktest <- function(ticker1, ticker2, capital = 100000, threshold = 1.5,
                           startTrain = Sys.Date() - 365*2, endTrain = Sys.Date() - 365,
                           startTest = Sys.Date() - 365, endTest = Sys.Date()) {
-  
+
   account_values <- c()
 
   #Get price data
@@ -34,6 +34,7 @@ PairsBacktest <- function(ticker1, ticker2, capital = 100000, threshold = 1.5,
   b_train <- b_close[dates <= endTrain & dates >= startTrain]
 
   model <- lm(log(a_train) ~ log(b_train))
+  res <- as.vector(residuals(model))
   mean_resid <- mean(res)
   sd_resid <- sd(res)
   intercept <- model$coefficients[1]
@@ -95,44 +96,44 @@ PairsBacktest <- function(ticker1, ticker2, capital = 100000, threshold = 1.5,
 
     account_values <- c(account_values, acct_val)
   }
-  
+
   data <- data.frame(Date=testDates, account_values=account_values, residuals=residual)
   data$Date = as.Date(data$Date)
-  
+
   result <- list()
   result$ticker1 <- ticker1
   result$ticker2 <- ticker2
-  
+
   result$data <- data
   result$model <- model
   result$lower_bound <- lower_bound
   result$upper_bound <- upper_bound
-  
+
   trainData <- stock_prices[stock_prices$Date %in% trainDates,]
   testData <- stock_prices[stock_prices$Date %in% testDates,]
-  
+
   colnames(trainData) <- c("Date", "a_train", "b_train")
   colnames(testData) <- c("Date", "a_test", "b_test")
-  
+
   trainData$a_train <- log(trainData$a_train)
   trainData$b_train <- log(trainData$b_train)
   testData$a_test <- log(testData$a_test)
   testData$b_test <- log(testData$b_test)
-  
+
   result$trainData <- trainData
   result$testData <- testData
-  
+
   result$model <- model
-  
+
   return(result)
-  
+
 }
 
 ggplotRegression <- function (fit) {
-  
+
   require(ggplot2)
-  
-  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) +
     geom_point() +
     stat_smooth(method = "lm", col = "red") +
     labs(title = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
@@ -158,7 +159,7 @@ sharpeRatio <- function(account_balances) {
   return(annualizeReturns(account_balances))/annualizeVolatility(account_balances)
 }
 
-backtest <- PairsBacktest('MSFT', 'AAPL')
+# backtest <- PairsBacktest('MSFT', 'AAPL')
 backtest_plot <- function(backtest){
   data <- backtest$data
   lower_bound <- backtest$lower_bound
@@ -166,25 +167,26 @@ backtest_plot <- function(backtest){
   acct_vals <- data$account_values
   summary_df=data.frame(Benchmark=c("Annualized Returns", "Annualized Volatility", "Sharpe Ratio"),
                         Result=c(annualizeReturns(acct_vals), annualizeVolatility(acct_vals), sharpeRatio(acct_vals)))
-  
-  acct_plot <- ggplot(data, aes(x=Date, y=account_values)) + geom_line(size=1) + xlab("Date") + 
-    ylab("Account Values") + ggtitle("Account Value over Time") 
-  
+
+  acct_plot <- ggplot(data, aes(x=Date, y=account_values)) + geom_line(size=1) + xlab("Date") +
+    ylab("Account Values") + ggtitle("Account Value over Time")
+
   summary_table <- tableGrob(summary_df, rows=NULL)
   account_value_grid <- grid.arrange(acct_plot,summary_table, nrow=2, as.table=T)
-  
-  residuals_plot <- ggplot(data, aes(x=Date, y=residuals)) + geom_line(size=1) + xlab("Date") + ylab("Residual") + 
-    ggtitle("Residuals over Time") + 
+
+  residuals_plot <- ggplot(data, aes(x=Date, y=residuals)) + geom_line(size=1) + xlab("Date") + ylab("Residual") +
+    ggtitle("Residuals over Time") +
     geom_hline(yintercept=c(lower_bound, upper_bound), linetype='dashed', colour="#CC0000", size=2)
-  
+
   regression_plot <- ggplot(data = backtest$trainData, aes(x=a_train, y=b_train)) +
     geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x) +
-    geom_point()
-  
+    geom_point() + ggtitle("Regression") + xlab(paste("LN(",backtest$ticker1,")")) +
+    ylab(paste("LN(", backtest$ticker2, ")"))
+
   plot_list <- list()
   plot_list$account_value_grid <- account_value_grid
   plot_list$residuals_plot <- residuals_plot
   plot_list$regression_plot <- regression_plot
   return(plot_list)
-} 
+}
 
